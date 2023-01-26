823a50d35ec38b531be7a8edc2368d2082f6096c
Holden Matthews <holdenmatthews1@gmail.com>
2023-01-26 14:20:10 -0700

    rollbar installed

--- server.js
+++ server.js
@@ -3,3 +3,14 @@ const app = express()
 app.listen(4000, () => console.log('server running on port 4000'))
 
 app.use(express.static(`${__dirname}/public`))
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'bce6a273a3c84059ae0571afbf02ec40',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
