76f36865f4eab01432c1151f3038cbbe0082de52
Holden Matthews <holdenmatthews1@gmail.com>
2023-01-26 15:29:49 -0700

    url changed

--- public/main.js
+++ public/main.js
@@ -1,7 +1,7 @@
 const bigRedButton = document.getElementById('bigRedButton')
 
 const scaryFace = () => {
-    axios.get("http://localhost:4000/scary-face")
+    axios.get("http://ec2-34-219-108-153.us-west-2.compute.amazonaws.com/scary-face")
     .then((res) => {
         document.body.innerHTML = `<img src="${res.data}">`
         
